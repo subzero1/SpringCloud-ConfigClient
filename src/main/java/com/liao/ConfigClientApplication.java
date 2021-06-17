43cf5c5da94f1612a04ceacebd3c9bc4d6fee1d4
jack <liaojk@gz2000.net>
2021-06-17 10:31:47 +0800

    解决spring cloud 2020版本以上禁用bootstarap的问题

--- src/main/java/com/liao/ConfigClientApplication.java
+++ src/main/java/com/liao/ConfigClientApplication.java
@@ -2,21 +2,12 @@ package com.liao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
-    }
-
-//    @Value("${data}") // git配置文件里的key
-    String data;
-
-    @RequestMapping(value = "/hi")
-    public String hi(){
-        return data;
     }
 
 }
