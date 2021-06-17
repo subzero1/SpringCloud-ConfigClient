0ba0186678a763d9187a090a7be593962ff4296b
jack <liaojk@gz2000.net>
2021-06-17 14:17:53 +0800

    本地配置热刷新

--- src/main/java/com/liao/GitController.java
+++ src/main/java/com/liao/GitController.java
@@ -1,10 +1,12 @@
 package com.liao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GitController {
 
     @Autowired
